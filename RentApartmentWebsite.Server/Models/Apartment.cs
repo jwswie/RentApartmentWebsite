868853a36b2aaabf4711a3253738a21931ef994f
jwswie <121186521+jwswie@users.noreply.github.com>
2025-05-13 15:30:57 +0200

    Changed Apartment Model

--- RentApartmentWebsite.Server/Models/Apartment.cs
+++ RentApartmentWebsite.Server/Models/Apartment.cs
@@ -1,13 +1,59 @@
-﻿namespace RentApartmentWebsite.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentApartmentWebsite.Server.Models
 {
     public class Apartment
     {
         public int ApartmentID { get; set; }
-        public string? ApartmentName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string ApartmentName { get; set; }
         public double ApartmentPrice { get; set; } = 0.0;
-        public string? ApartmentLocation { get; set; }
-        public string? ApartmentCountry { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string ApartmentLocation { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string ApartmentCountry { get; set; }
+
         public double ApartmentRate { get; set; } = 0.0;
-        public string? ApartmentPhoto { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string ApartmentPhoto { get; set; }
+
+        [Required]
+        public int ApartmentArea { get; set; }
+
+        [Required]
+        public int ApartmentCapacity { get; set; }
+
+        [Required]
+        public int Bedroom { get; set; }
+
+        [Required]
+        public int Bathroom { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string AllowedEvents { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string AllowedChildren { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string AllowedPets { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string AllowedSmoking { get; set; }
+
+        [Required]
+        public int OwnerID { get; set; }
     }
 }
